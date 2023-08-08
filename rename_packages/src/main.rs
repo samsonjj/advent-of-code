0058eac7edc852c072993ae3b2d365b96d0a42e4
Jonathan Samson <jonathansamsonvcu@gmail.com>
2023-08-07 20:15:49 -0400

    before package renaming

--- rename_packages/src/main.rs
+++ rename_packages/src/main.rs
@@ -1,22 +1,51 @@
-fn main() {
-    println!("Hello, world!");
-    let dirs = std::fs::read_dir("./").unwrap();
-    for dir in dirs.into_iter() {
-        let dir = dir.unwrap();
-        let sub_dirs = std::fs::read_dir(dir.path()).unwrap();
-        for sub_dir in sub_dirs.into_iter() {
-            let sub_dir = sub_dir.unwrap();
-            let entries = std::fs::read_dir(sub_dir.path()).unwrap();
-            for entry in entries.into_iter() {
-                let entry = entry.unwrap();
-                if !entry.file_type().unwrap().is_file() {
-                    continue;
-                }
-                if entry.file_name() != "Cargo.toml" {
-                    continue;
-                }
-                let contents = std::fs::read_to_string(entry.path()).unwrap();
-            }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let current_dir = std::env::current_dir().unwrap();
+    find_subdirectories(&current_dir)?;
+    Ok(())
+}
+
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+
+fn find_subdirectories(root_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    for entry in fs::read_dir(root_dir).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if !path.is_dir() {
+            continue;
         }
+        let Some(subdir_name) = path.file_name().and_then(|s| s.to_str()) else {
+            continue;
+        };
+        if !subdir_name.starts_with("day-") {
+            continue;
+        }
+        process_package(&path, entry.file_name().to_str().unwrap())?;
     }
+
+    Ok(())
+}
+
+use regex::Regex;
+
+fn process_package(path: &Path, year: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let cargo_file = path.join("Cargo.toml");
+
+    let cargo_contents = fs::read_to_string(&cargo_file)?;
+    let package_name_pattern = Regex::new(r#"name\s*=\s*"(day-\d+)""#)?;
+
+    let replacer = |captures: &regex::Captures| -> String {
+        let capture = captures.get(1).unwrap();
+        let result = format!("{}-{}", capture.as_str(), year);
+        result
+    };
+
+    let updated_cargo_contents =
+        package_name_pattern.replace_all(cargo_contents.as_str(), replacer);
+
+    let mut new_cargo_file = fs::File::create(&path)?;
+    new_cargo_file.write_all(updated_cargo_contents.as_bytes())?;
+
+    Ok(())
 }
